1973109a556c35274654aa9f90e635c9d09893a0
root <you@example.com>
2024-02-14 16:47:15 -0800

    lll

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit t."""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
@@ -10,7 +10,19 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/himza25)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 404:
+        print("Output: Non-existing subreddit")
         return 0
+
     results = response.json().get("data")
-    return results.get("subscribers")
+    subscribers = results.get("subscribers")
+    print("Output: Existing subreddit")
+    print(f"Number of subscribers: {subscribers}")
+
+    return subscribers
+
+
+# Example usage:
+subreddit_name = "python"  # Replace with the subreddit you want to check
+number_of_subscribers(subreddit_name)
