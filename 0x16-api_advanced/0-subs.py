d9a1c103fe273ccf646469c22c0a34dc763abcc2
root <you@example.com>
2024-02-14 16:56:37 -0800

    lll

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit t."""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
     "User-Agent": "linux:mon_bot_reddit:1.0.0:/u/axcis380"
-}
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 404:
         return 0
-
     results = response.json().get("data")
     return results.get("subscribers")
