f41e0dde432bcb6f852ef5725ce941fa60d91e66
root <you@example.com>
2024-02-14 17:25:11 -0800

    updt

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'MyRedditApp/0.1 (by /u/himza25)'}
+    headers = {'User-Agent': 'MyRedditApp/0.1 (by /u/Axcis380'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
