68e6c427080b9090f406c79b503f790e6e5a09b8
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2024-02-22 23:18:56 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
 # Fixes an nginx site that can't handle multiple concurrent requests
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
+
+#modify the ultimate value
   command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
 /etc/default/nginx; service nginx restart\"",
+
+# specify the path for the sed command
   path    => '/usr/bin:/usr/sbin:/bin'
 }
