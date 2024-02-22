68bb251ab5c223c16dd7cfdf666cc2452054c7f3
axcis 380 <anas.baidouri@gmail.com>
2024-02-22 22:13:52 +0000

    updt0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,6 @@
-# Increases the amount of traffic an Nginx server can handle.
-
-# Increase the ULIMIT of the default file
+# Fixes an nginx site that can't handle multiple concurrent requests
 exec { 'fix--for-nginx':
-#modify the ultimate value 
-  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-# specify the path for the sed command 
- path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-# restart nginx service 
- command => '/etc/init.d/nginx restart',
-# Specify the path for the init.d script
-  path    => '/etc/init.d/'
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
