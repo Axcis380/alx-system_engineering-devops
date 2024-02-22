9a46f9f5c0bb34cf0f963d93d2c244f470eadc88
axcis 380 <anas.baidouri@gmail.com>
2024-02-22 21:55:24 +0000

    updt

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,16 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
+#modify the ultimate file
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+# specify the path for sed cmd 
+ path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => '/etc/init.d/nginx resart',
+# restart nginx serv 
+ command => '/etc/init.d/nginx restart',
+# Specify the path for init.d scp
   path    => '/etc/init.d/'
 }
